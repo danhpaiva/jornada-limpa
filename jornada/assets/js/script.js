629a00b3291978fe3a83d1b69d2e8d51adb6e4c8
Paiva <danpaiva@live.com>
2025-10-16 00:13:02 -0300

    feat: testando cicd

--- jornada/assets/js/script.js
+++ jornada/assets/js/script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const dataInicio = new Date('2025-10-10T00:00:00');
+  const dataInicio = new Date('2025-10-16T00:00:00');
   const elementoDiasLimpos = document.getElementById('dias-limpos');
   const elementoMensagem = document.getElementById('mensagem');
 
