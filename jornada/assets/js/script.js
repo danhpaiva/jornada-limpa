bc4b7f5d1686952dd70bcb306904a8661a18318a
Paiva <danpaiva@live.com>
2025-12-07 18:06:46 -0300

    refact: test ci/cd

--- jornada/assets/js/script.js
+++ jornada/assets/js/script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const dataInicio = new Date('2025-11-30T00:00:00');
+  const dataInicio = new Date('2025-12-08T00:00:00');
   const elementoDiasLimpos = document.getElementById('dias-limpos');
   const elementoMensagem = document.getElementById('mensagem');
 
