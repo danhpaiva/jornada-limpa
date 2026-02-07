26994ea13826cad2899dedbb6f3ea543056567f8
Paiva <danpaiva@live.com>
2026-02-07 13:15:55 -0300

    refact: teste cicd

--- jornada/assets/js/script.js
+++ jornada/assets/js/script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const dataInicio = new Date('2026-02-02T00:00:00');
+  const dataInicio = new Date('2026-02-07T00:00:00');
   const elementoDiasLimpos = document.getElementById('dias-limpos');
   const elementoMensagem = document.getElementById('mensagem');
 
